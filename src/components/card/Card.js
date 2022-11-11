d0c49311229b4a16f56977c0ebed23ba4ec0cb52
betrayalove <Android620072@yandex.ru>
2022-11-11 18:16:51 +0500

    buti

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import '../../index.css'
+import './buy.css'
 import ReactAudioPlayer from "react-audio-player";
 import sound1 from "../../sounds/1.mp3";
 
 function Card(props) {
-    const [isPlaying, setIsPlaying] = useState(false)
+    // const [isPlaying, setIsPlaying] = useState(false)
 
     return (
         <body>
@@ -17,14 +18,14 @@ function Card(props) {
                         <p className='author'>{props.author.toUpperCase()}</p>
                         <p className='title'>{props.title.toLowerCase()}</p>
                         <p className='totalTime'>{props.time}</p>
-                        <ReactAudioPlayer
+                        <p className='player'><ReactAudioPlayer
                             src={sound1}
                             autoPlay={false}
                             controls
                             onAbort={true}
                             onCanPlay
                             volume={0.5}
-                        />
+                        /></p>
                     </li>
                     {/*<li className='el'><img className='playButton' width={50} height={50} src={isPlaying ? Pause : Play}*/}
                     {/*         onClick={() => {*/}
